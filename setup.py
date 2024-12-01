c024aed6f13683e03f4f3ea653c7cc1630aad429
BOTTOMFRAGGER523 <dastancoding@gmail.com>
2024-12-01 12:16:51 +0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_package",  # The name of your package
-    version="0.1.0",  # The version of your package
+    name="markdownrenderer",  # The name of your package
+    version="1.0",  # The version of your package
     packages=find_packages(),  # Automatically find packages in the directory
     install_requires=[
         'pywebview',
@@ -12,7 +12,6 @@ setup(
     description="A brief description of your package",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/my_package"
+    author="BOTTOMFRAGGER523",
+    url="https://github.com/BOTTOMFRAGGER523/Markdown-RendererPy"
 )
